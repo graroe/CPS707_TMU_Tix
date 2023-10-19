e524e4d818ebf8887b800265bfe781fd76c4cfae
graroe <graroe@gmail.com>
2023-10-19 16:56:28 -0400

    add event class

---
+++ front_end/event.py
@@ -0,0 +1,7 @@
+class event():
+    def __init__(self, name, date, avail_tickets = 0, new_tickets = 0):
+        self.name = name
+        self.date = date
+        self.avail_tickets = avail_tickets
+        self.new_tickets = new_tickets
+        
